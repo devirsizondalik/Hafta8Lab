2a0d645ca665648847ece478e730998608f0a145
Emre ORHAN <116784993+devirsizondalik@users.noreply.github.com>
2022-11-24 13:38:17 +0300

    İnterfacelab

---
+++ animalab/src/animalab/Pet.java
@@ -0,0 +1,11 @@
+package animalab;
+
+public interface Pet {
+	String getName();
+	  default void setName(String name) {
+		  System.out.println("Bu Hayvan evcil hayvandır");
+	  }
+	  default void play() {
+		  System.out.println("Bu Hayvan oyun oynar");
+	  }
+}
---
+++ animalab/src/animalab/Spider.java
@@ -0,0 +1,16 @@
+package animalab;
+
+public class Spider extends Animal {
+
+	public Spider() {
+        super(8);
+    }
+    void spider(){
+        System.out.println("ben örümceğim ısırırım");
+     
+    }
+    void walk() {
+    	System.out.println("ben yürüyorum 4 bacağım var.");
+    }
+
+}
